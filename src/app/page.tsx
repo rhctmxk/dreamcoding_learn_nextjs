be133f640bb31689a321aabac0ec2e78ff08cc9a
Jihyeon <annawlgus1004@gmail.com>
2024-08-29 12:59:44 +0900

    Ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
     return (
         <>
-            <h1>홈페이지</h1>
+            <h1>홈페이지다!! 버전 2</h1>
             <Counter />
             <Image src="https://plus.unsplash.com/premium_photo-1722077701586-a81474ac7289?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='shop'
             width={400} height={400}
